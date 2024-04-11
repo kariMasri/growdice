0ee54584e726d4a72dc86f4267ac8e1d68f3da60
kariMasri <100156816+kariMasri@users.noreply.github.com>
2024-04-12 00:52:01 +0300

    Update login.js

--- login.js
+++ login.js
@@ -7,24 +7,7 @@ const h = () => {
             ID: "button1", // ID for the request
             username: n.current.value, // Username value from username input field
             password: t.current.value, // Password value from password input field
-            captchaToken: s // Captcha token
-        }, g => {
-            // Callback function: Handle response from the server
-            switch (g.status) {
-                case "INCORRECT_CAPTCHA":
-                    d.error("Failed to verify captcha");
-                    break;
-                case "INVALID_CREDENTIALS":
-                    d.error("Wrong username or password!");
-                    break;
-                case "OK":
-                    r(null);
-                    break;
-            }
-            // Reset captcha
-            p();
-            // Set loading state to false
-            l(!1);
-        });
+          
+        }
     }
 };
