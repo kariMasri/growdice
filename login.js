aa9b2e16e129c1d092c96e9100b1147ed96d3138
kariMasri <100156816+kariMasri@users.noreply.github.com>
2024-04-12 00:47:02 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,8 +1,30 @@
-const form = document.querySelector("form")
-
-form.addEventListener("submit",(e)=>{
-    e.preventDefault()
-
-    const username = form.username.value
-    const password = form.password.value
-}
+const h = () => {
+    if (s.length < 1 || !n.current || !t.current) {
+        // Condition check: captcha token present, username and password fields are not empty
+        l(!0); // Set loading state to true
+        // Send login request
+        b.sendWithCallback({
+            ID: "button1", // ID for the request
+            username: n.current.value, // Username value from username input field
+            password: t.current.value, // Password value from password input field
+            captchaToken: s // Captcha token
+        }, g => {
+            // Callback function: Handle response from the server
+            switch (g.status) {
+                case "INCORRECT_CAPTCHA":
+                    d.error("Failed to verify captcha");
+                    break;
+                case "INVALID_CREDENTIALS":
+                    d.error("Wrong username or password!");
+                    break;
+                case "OK":
+                    r(null);
+                    break;
+            }
+            // Reset captcha
+            p();
+            // Set loading state to false
+            l(!1);
+        });
+    }
+};
